d234d244d090f079f851c7801bfe7daf0388be78
cisceara <cisceara@gmail.com>
2024-08-14 02:24:23 -0300

    responsivoEdit

--- app.js
+++ app.js
@@ -1,4 +1,4 @@
-// Mobile menu ------------------------------------------------
+// MOBILE MENU ------------------------------------------------
 const hamburger = document.querySelector('.hamburger');
 const navbar = document.querySelector('.navbar');
 const links = document.querySelectorAll('.links .link');
@@ -28,7 +28,6 @@ let next = document.getElementById('next');
 let prev = document.getElementById('prev');
 let thumbnails = document.querySelectorAll('.thumbnail .item');
 
-// parâmetros
 let countItem = items.length;
 let itemActive = 0;
 
@@ -50,7 +49,7 @@ prev.onclick = function () {
 }
 let refreshInterval = setInterval(() => {
     next.click();
-}, 10000)
+}, 9000)
 function showhome() {
 
     let itemActiveOld = document.querySelector('.box-banners .list .item.active');
@@ -64,7 +63,7 @@ function showhome() {
     clearInterval(refreshInterval);
     refreshInterval = setInterval(() => {
         next.click();
-    }, 10000)
+    }, 9000)
 }
 
 thumbnails.forEach((thumbnail, index) => {
@@ -73,6 +72,64 @@ thumbnails.forEach((thumbnail, index) => {
         showhome();
     })
 });
+
+//PROMOÇÃO ------------------------------------------------
+var TxtType = function(el, toRotate, period) {
+    this.toRotate = toRotate;
+    this.el = el;
+    this.loopNum = 0;
+    this.period = parseInt(period, 10) || 2000;
+    this.txt = '';
+    this.tick();
+    this.isDeleting = false;
+};
+
+TxtType.prototype.tick = function() {
+    var i = this.loopNum % this.toRotate.length;
+    var fullTxt = this.toRotate[i];
+
+    if (this.isDeleting) {
+    this.txt = fullTxt.substring(0, this.txt.length - 1);
+    } else {
+    this.txt = fullTxt.substring(0, this.txt.length + 1);
+    }
+
+    this.el.innerHTML = '<span class="wrap">'+this.txt+'</span>';
+
+    var that = this;
+    var delta = 200 - Math.random() * 100;
+
+    if (this.isDeleting) { delta /= 2; }
+
+    if (!this.isDeleting && this.txt === fullTxt) {
+    delta = this.period;
+    this.isDeleting = true;
+    } else if (this.isDeleting && this.txt === '') {
+    this.isDeleting = false;
+    this.loopNum++;
+    delta = 500;
+    }
+
+    setTimeout(function() {
+    that.tick();
+    }, delta);
+};
+
+window.onload = function() {
+    var elements = document.getElementsByClassName('typewrite');
+    for (var i=0; i<elements.length; i++) {
+        var toRotate = elements[i].getAttribute('data-type');
+        var period = elements[i].getAttribute('data-period');
+        if (toRotate) {
+          new TxtType(elements[i], JSON.parse(toRotate), period);
+        }
+    }
+
+    var css = document.createElement("style");
+    css.type = "text/css";
+    css.innerHTML = ".typewrite > .wrap { border-right: 0.08em solid #fff}";
+    document.body.appendChild(css);
+};
 
 // SERVIÇOS ------------------------------------------------
 
